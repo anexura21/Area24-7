3e55f1f86bbb6ed9ee291caaedd5b182d67e044a
Sebastian Pinto Becerra <sebastianpintobecerra7@gmail.com>
2021-02-17 23:58:27 -0500

    Se prueba con exito el cargue de imagenes para reportar avistamiento

--- src/app/providers/asombrate.service.ts
+++ src/app/providers/asombrate.service.ts
@@ -1,16 +1,27 @@
 import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { data } from "jquery";
+import { from, Observable, Observer } from "rxjs";
 import { map, timeout } from "rxjs/operators";
 import { Especie } from "../entities/especie";
 import { EspecieSugeridaIA } from "../entities/especieSugeridaIA";
 import { CONFIG_ENV } from "../shared/config-env-service/const-env";
+import {
+  FileTransfer,
+  FileTransferObject,
+  FileUploadOptions,
+  FileUploadResult,
+} from "@ionic-native/file-transfer/ngx";
 
 @Injectable({
   providedIn: "root",
 })
 export class AsombrateService {
-  constructor(private http: HttpClient) {}
+  private fileTransferObject: FileTransferObject;
+
+  constructor(private http: HttpClient, private fileTransfer: FileTransfer) {
+    this.fileTransferObject = this.fileTransfer.create();
+  }
 
   obtenerSugerenciaAvistamiento(objetoImagen): Observable<any> {
     const url = `https://webservices.metropol.gov.co/SIMAPI2/api/Asombrate/ObtenerSugerencia`;
@@ -33,44 +44,86 @@ export class AsombrateService {
     especieReportada: Especie,
     foto: { id: number; url: string },
     geoposition: {
-      lat: number;
-      lng: number;
+      lat: any;
+      lng: any;
     }
-  ): Observable<any> {
+  ) {
     const url = CONFIG_ENV.REST_BASE_URL + "/api/avistamiento";
-    const headers = new HttpHeaders({
-      "Content-Type": "application/json",
-      Authorization: localStorage.getItem("bearer"),
-    });
+    const bearer = localStorage.getItem("bearer");
+    const imageBlob = this.dataURItoBlob(foto?.url.split(",")[1]);
+    const reader = new FileReader();
 
-    const blob = new Blob([foto?.url]);
-    const formData = new FormData();
-    const file = new File([blob], "avistamiento.jpeg");
-    formData.append("image", file);
+    reader.onload = () => {
+      const formData = new FormData();
+      const imageName = "avistamiento.png";
+      const imgBlob = new Blob([reader.result], { type: "image/png" });
+      formData.append("multimedia", imgBlob, imageName);
 
-    const parametrosServicio = {
-      nombreComun: especieReportada.nombreComun,
-      descripcion: especieReportada.descripcion,
-      nombreCientifico: especieReportada.nombreCientifico,
-      username: localStorage.getItem("username"),
-      nivelCapa: "CAPA",
-      idCapaCategoria: "487",
-      latitud: geoposition?.lat.toString(),
-      longitud: geoposition?.lng.toString(),
-      recorridoArbol: "",
+      const headers = new HttpHeaders({
+        //"Content-Type": "application/json",
+        //"Content-Type": "multipart/form-data",
+        mimeType: 'multipart/form-data',
+        // "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: bearer !== null ? bearer : "",
+        processData: "false",
+      });
+
+      const parametrosServicio = {
+        nombreComun: especieReportada.nombreComun,
+        descripcion: especieReportada.descripcion,
+        nombreCientifico: especieReportada.nombreCientifico,
+        username: localStorage.getItem("username"),
+        nivelCapa: "CAPA",
+        idCapaCategoria: "487",
+        latitud: geoposition?.lat.toString(),
+        longitud: geoposition?.lng.toString(),
+        recorridoArbol: " ",
+      };
+
+      const paramsCustom = new HttpParams({ fromObject: parametrosServicio });
+
+      console.log(url, formData, formData.get("file"), {
+        headers,
+        params: paramsCustom,
+      });
+
+      this.http
+        .post(url, formData, { headers, params: paramsCustom })
+        .subscribe(
+          (resposne) => {
+            console.log(resposne);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
     };
 
-    const params = new HttpParams({ fromObject: parametrosServicio });
+    reader.readAsArrayBuffer(imageBlob);
+  }
 
-    console.log(formData, { headers, params });
+  private dataURItoBlob(dataURI) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([int8Array], { type: "image/png" });
+    return blob;
+  }
 
-    return this.http
-      .post(url, { multimedia: formData }, { headers, params })
-      .pipe(
-        map((resposne) => {
-          return resposne;
-        }),
-        timeout(9000)
-      );
+  uploadMedia(urlMedia: string, url: string) {
+    const options: FileUploadOptions = {
+      fileKey: "multimedia",
+      mimeType: "multipart/form-data",
+      headers: { Authorization: localStorage.getItem("bearer") },
+    };
+
+    from(
+      this.fileTransferObject.upload(urlMedia, encodeURI(url), options)
+    ).subscribe((response) => {
+      console.log("uploadMedia", response);
+    });
   }
 }
